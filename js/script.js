153d0e4a319125de5c66c2a67593b6b52714e7a7
ArmeethGarry <armeeth2007@yandex.ru>
2021-02-24 12:20:25 +0300

    добавление class CCS с помощью Rest оператора

--- js/script.js
+++ js/script.js
@@ -156,17 +156,24 @@ document.addEventListener( 'DOMContentLoaded', () => {
 
     // Создаем class который создает карточки Меню
     class createMenuCard {
-        constructor( src, alt, title, descr, price, parentSelector) {
+        constructor( src, alt, title, descr, price, parentSelector, ...classesCSS) {
             this.src = src,
             this.alt = alt,
             this.title = title,
             this.descr = descr,
             this.price = price,
+            this.classesCSS = classesCSS,
             this.parent = document.querySelector( parentSelector )
         }
 
         create() {
             const element = document.createElement( 'div' );
+            if ( this.classesCSS.length === 0 ) {
+                this.element = 'menu__item';
+                element.classList.add( this.element );
+            } else {
+                this.classesCSS.forEach( className => element.classList.add( className ) );
+            }
 
             element.innerHTML = `
             <div class="menu__item">
@@ -194,7 +201,8 @@ document.addEventListener( 'DOMContentLoaded', () => {
         'Меню "Фитнес"',
         'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!',
         600,
-        '.menu .container'
+        '.menu .container',
+        'menu__item'
     ).create();
     
     // Карточка Меню - Премиум
@@ -204,7 +212,8 @@ document.addEventListener( 'DOMContentLoaded', () => {
         'Меню “Премиум”',
         'В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!',
         1500,
-        '.menu .container'
+        '.menu .container',
+        'menu__item'
     ).create();
 
     // Карточка Меню - Постное
@@ -214,6 +223,7 @@ document.addEventListener( 'DOMContentLoaded', () => {
         'Меню "Постное"',
         'Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков.',
         1100,
-        '.menu .container'
+        '.menu .container',
+        'menu__item'
     ).create();
  });
